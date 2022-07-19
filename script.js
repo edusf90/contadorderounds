4204f6ab037d5d76e01e317a9fa55026ed0ee11c
Eduardo Santana <edusf0@hotmail.com>
2022-07-19 10:46:49 -0300

    adicionado botao -1

--- script.js
+++ script.js
@@ -8,6 +8,15 @@ function contar() {
   console.log(contadorNumero)
 }
 
+function diminiuir() {
+  if (contadorNumero <= 0) {
+    contadorNumero = 0
+  } else {
+    contadorNumero = contadorNumero - 1
+  }
+  document.getElementById('contador').innerHTML = contadorNumero
+}
+
 function Resetar() {
   contadorNumero = 0
   document.getElementById('contador').innerHTML = contadorNumero
